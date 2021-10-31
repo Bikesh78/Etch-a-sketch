8ccb0346fb7d26dc68578a4a77d3fb2e3063b726
Bikesh Rai <bikeshrai78@gmail.com>
2021-10-31 19:31:38 +0545

    Add clear colour button

--- JS/script.js
+++ JS/script.js
@@ -2,24 +2,32 @@ const body = document.querySelector('body');
 const container = document.createElement('div');
 container.classList.add('container');
 body.appendChild(container);
-const grid= document.createElement('div');
-grid.classList.add('grid');
+const grids= document.createElement('div');
+grids.classList.add('square');
 // creage new div element and append it to container each time
 for (let i=0; i<16*16; i++){
-    const grid= document.createElement('div');
-    grid.classList.add('grid');
-    container.appendChild(grid);
+    const grids= document.createElement('div');
+    grids.classList.add('square');
+    container.appendChild(grids);
 }
 container.addEventListener('mouseover',function(e){
-    console.log(e.target.classList);
-    if(e.target.classList.value === 'grid'){
+   // console.log(e.target.classList);
+    
+    if(e.target.classList.value === 'square'){
         e.target.style.backgroundColor = "black";
     }
    
 });
+console.log(grids);
 const clear = document.createElement('button');
 clear.classList.add('clear');
 clear.textContent= 'Clear';
-
+clear.addEventListener('click', function(){
+    colorOfGrid = document.querySelectorAll('.square');
+   // console.log(colorOfGrid);
+    colorOfGrid.forEach(square =>{
+        square.style.backgroundColor ="";
+    });
+});
 
 body.appendChild(clear);
